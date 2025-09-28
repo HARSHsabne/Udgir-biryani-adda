1daae30a505a2bf996c2442d02780768846e4004
HARSHsabne <108685939+HARSHsabne@users.noreply.github.com>
2025-09-28 14:50:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'udgir-biryani-v5'; // Increment the version when updating
+const CACHE_NAME = 'udgir-biryani-v6'; // Increment the version when updating
 
 const urlsToCache = [
   '/',
